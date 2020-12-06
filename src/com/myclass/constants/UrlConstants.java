169856614ffd0df97734b98cd804eba86e9724f9
Hai1820 <59417083+Hai1820@users.noreply.github.com>
2020-12-06 18:49:03 +0700

    src

---
+++ src/com/myclass/constants/UrlConstants.java
@@ -0,0 +1,21 @@
+package com.myclass.constants;
+
+public class UrlConstants {
+//	role constants
+
+	public static final String URL_ROLE = "/role";
+	public static final String URL_ROLE_ADD = "/role/add";
+	public static final String URL_ROLE_EDIT = "/role/edit";
+	public static final String URL_ROLE_DELETE = "/role/delete";
+//	user constants
+	public static final String URL_USER = "/user";
+	public static final String URL_USER_ADD = "/user/add";
+	public static final String URL_USER_EDIT = "/user/edit";
+	public static final String URL_USER_DELETE = "/user/delete";
+//	task controller
+	public static final String URL_TASK = "/task";
+//	job constants
+	public static final String URL_JOB="/job";
+	public static final String URL_JOB_ADD="/job/add";
+	
+}
